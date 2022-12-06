b22f15a7076869aa4e3257a347a59ec0f52c0a74
lynsiearagon <lynsie.aragon@gmail.com>
2022-12-05 22:13:38 -0500

    HashSet Walkthrough with code

--- Hash_map/lib/p03_hash_set.rb
+++ Hash_map/lib/p03_hash_set.rb
@@ -7,12 +7,20 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key)
+    self[key.hash] << key
+    self.count += 1 
+    resize! if num_buckets < self.count 
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    return nil unless include?(key)
+    self[key.hash].delete(key)
+    self.count -= 1
   end
 
   private
@@ -26,5 +34,19 @@ class HashSet
   end
 
   def resize!
+    old_store = self.store
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) { Array.new }
+
+    # old_store.flatten.each { |key| insert(key) } # This has more compute time
+    
+    # Less runtime 
+
+    old_store.each do |bucket|
+      bucket.each do |key|
+        insert(key)
+      end
+    end
+
   end
 end
